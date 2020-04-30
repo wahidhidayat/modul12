4ffb7a8cdabfd6a8f9bd6feb4a35d6edc20809dd
wahidhidayat <wahidhidayat3899@gmail.com>
2020-04-30 07:47:35 +0700

    modul 12

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBXQAjETl0WjtOfNlMHRjSHYHvMo2-2exU",
+    authDomain: "fir-login-cce0c.firebaseapp.com",
+    databaseURL: "https://fir-login-cce0c.firebaseio.com",
+    projectId: "fir-login-cce0c",
+    storageBucket: "fir-login-cce0c.appspot.com",
+    messagingSenderId: "166523565570",
+    appId: "1:166523565570:web:d52cf656806747c3f1991b",
+    measurementId: "G-SYXZ4YEPZE"
+};
+
+export default firebaseConfig;
